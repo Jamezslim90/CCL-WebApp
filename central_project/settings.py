01399c74001d34e6bfcbeeb78f882dbbbf5c37ef
Sunday James <jamezslim90@gmail.com>
2024-01-24 19:13:16 +0100

    allowed host update

--- central_project/settings.py
+++ central_project/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-pis0j&rsl*2h*--199!)mjdm-lclp0^#8b4=6+7bv=w-$4(=b^
 DEBUG = True
 
 # ALLOWED_HOSTS = ["*"]
-ALLOWED_HOSTS = ["*", "localhost", "0.0.0.0", "127.0.0.1",]
+ALLOWED_HOSTS = ["*", "localhost", "0.0.0.0", "127.0.0.1", "ccl-6v3u.onrender.com"]
 
 
 
